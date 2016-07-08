3a735ffe8631f4c1100821dabdc77dc1cb31e15d
SGly94 <21425055@zju.edu.cn>
2016-07-08 11:19:47 +0800

    Add files via upload
    Programming assignment 2 lexical scoping の r- programming の Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## MakeCacheMatrix creates a special matrix object that can cache its inverse
+        inv <- NULL 
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(Inv) inv <<- Inv########
+        getInv <- function() inv
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Compute the -1 of the special matrix returned by MakeCacheMatrix
+## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv
+        if (!is.null(inv)){
+                message("getting cached data")
+                return (inv)
+        }
+        data <- x$get()
+        inv <- solve(data,...)
+        x$setInv(inv)
+        inv
 }
